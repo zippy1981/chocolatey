a6c5acc2a097005fa4086a14d0615e2d8ab2542b
Justin Dearing <zippy1981@gmail.com>
2012-11-09 10:11:47 -0500

    Updated ditto to 3.18.46. It now restarts the process.

--- ditto/tools/chocolateyInstall.ps1
+++ ditto/tools/chocolateyInstall.ps1
@@ -1,3 +1,9 @@
-Install-ChocolateyPackage 'ditto' 'exe' '/sp- /silent /norestart' 'https://sourceforge.net/projects/ditto-cp/files/Ditto/3.18.24.0/DittoSetup_3_18_24_0.exe/download' 'https://sourceforge.net/projects/ditto-cp/files/Ditto/3.18.24.0/DittoSetup_64bit_3_18_24_0.exe/download'
+# The installer doesn't properly kill ditto
+Get-Process | Where { $_.name -eq 'ditto' } | Stop-Process
+Install-ChocolateyPackage 'ditto' 'exe' '/sp- /silent /norestart' `
+	'https://sourceforge.net/projects/ditto-cp/files/Ditto/3.18.46.0/DittoSetup_3_18_46_0.exe/download' `
+	'https://sourceforge.net/projects/ditto-cp/files/Ditto/3.18.46.0/DittoSetup_64bit_3_18_46_0.exe/download'
 Set-ItemProperty -Path HKCU:\Software\Ditto\CopyStrings -name 'powershell.exe' -value '% {{Delay100}ey'
 Set-ItemProperty -Path HKCU:\Software\Ditto\PasteStrings -name 'powershell.exe' -value '% {{Delay100}ep'
+# It also doesn't start the process
+start (Get-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Run).Ditto
