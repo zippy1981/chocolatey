65245902365b2049d16cc0be4df103878d085077
Justin Dearing <zippy1981@gmail.com>
2013-01-13 00:40:31 -0500

    added inkscape

---
+++ Inkscape/tools/chocolateyInstall.ps1
@@ -0,0 +1,17 @@
+﻿try {
+  if (Test-Path HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Inkscape) {
+    $installedVersion = [Version] (Get-ItemProperty HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Inkscape DisplayVersion).DisplayVersion
+  }
+  if ($installedVersion -eq [Version] '0.48.4.0') {
+    Write-ChocolateySuccess 'inkscape' 'Inkscape is already installed. Updating the chococolatey database.'
+  }
+  elseif ($installedVersion -gt [Version] '0.48.4.0') {
+    Write-ChocolateyFailure 'inkscape' "A newer version of inkscape [$($installedVersion)] is already installed. Updating the chococolatey database."
+  }
+  else {
+    Install-ChocolateyPackage 'inkscape' 'exe' '/S' 'https://downloads.sourceforge.net/project/inkscape/inkscape/0.48.4/inkscape-0.48.4-1-win32.exe?use_mirror=switch'
+  }
+} catch {
+  Write-ChocolateyFailure 'inkscape' "$($_.Exception.Message)"
+  throw 
+}
---
+++ Inkscape/tools/chocolateyUninstall.ps1
@@ -0,0 +1,2 @@
+﻿$uninstallPath = (Get-ItemProperty HKLM:SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Inkscape UninstallString).UninstallString
+& $uninstallPath /S
