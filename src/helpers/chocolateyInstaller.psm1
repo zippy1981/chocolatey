18fb61435520c5fb45263f74180c84b62ae242c5
Justin Dearing <zippy1981@gmail.com>
2013-04-21 12:47:57 -0400

    Dnamically export module members

--- src/helpers/chocolateyInstaller.psm1
+++ src/helpers/chocolateyInstaller.psm1
@@ -3,10 +3,8 @@
 $DebugPreference = "SilentlyContinue"
 if ($env:ChocolateyEnvironmentDebug -eq 'true') {$DebugPreference = "Continue";}
 
-
 # grab functions from files
-Resolve-Path $helpersPath\functions\*.ps1 | 
-    ? { -not ($_.ProviderPath.Contains(".Tests.")) } |
-    % { . $_.ProviderPath }
+Get-Item $helpersPath\functions\*.ps1 | 
+    ? { -not ($_.Name.Contains(".Tests.")) } |
+    % { . $_.FullName;  if ( -not ($_.Name.Contains(".Internal.")))  { Export-ModuleMember -Function $_.BaseName } }
 
-Export-ModuleMember -Function Start-ChocolateyProcessAsAdmin, Install-ChocolateyPackage, Uninstall-ChocolateyPackage, Install-ChocolateyZipPackage, Install-ChocolateyPowershellCommand, Get-ChocolateyWebFile, Install-ChocolateyInstallPackage, Get-ChocolateyUnzip, Write-ChocolateySuccess, Write-ChocolateyFailure, Install-ChocolateyPath, Install-ChocolateyDesktopLink, Install-ChocolateyPinnedTaskBarItem, Install-ChocolateyExplorerMenuItem, Install-ChocolateyFileAssociation, Install-ChocolateyEnvironmentVariable, Write-Host, Write-Debug, Write-Error, Update-SessionEnvironment, Install-ChocolateyVsixPackage
